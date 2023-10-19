6b09471bc2b46a9705d94a838b004eb4a2f465ab
inderpal2002 <147585214+inderpal2002@users.noreply.github.com>
2023-10-19 05:51:55 +0000

    day2

---
+++ indr/test.js
@@ -0,0 +1,22 @@
+// console.log('my name is inder')
+
+
+// const id = 1234567
+// let email = "@inder" 
+// var accountpassword = 1234567
+// console.log(id,email,accountpassword)
+// console.log(typeof id)
+// console.log(typeof email)
+// console.log(typeof accountpassword)
+
+// let score = "32"
+// let valueinNumber =Number(score)
+// console.log(valueinNumber)
+// console.log(typeof valueinNumber)
+// console.log(typeof score)
+
+let a = true
+let b = String(a)
+console.log(typeof b)
+
+
